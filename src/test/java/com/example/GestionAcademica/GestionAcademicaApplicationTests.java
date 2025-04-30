c23794adcd264bceb647def6d85bf2ea742cd0e8
SantiagoEchavarria <santiagogil7112@gmail.com>
2025-04-30 17:02:21 -0500

    GestionAcademica

---
+++ src/test/java/com/example/GestionAcademica/GestionAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionAcademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
